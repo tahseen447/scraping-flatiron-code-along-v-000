dc40d5b1272a8f6a84a8371516ba697bc8a48dec
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-07-17 23:49:34 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
-  
+
     def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -15,11 +15,11 @@ class Scraper
       end
     end
   end
-  
+
   def get_page
-  end 
-  
+  end
+
+  def get_courses
+  end
+
 end
-
-
-
