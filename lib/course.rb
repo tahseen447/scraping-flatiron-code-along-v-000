cfa55674be7aa69820a8c01b61368a6bc05dd5cc
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-07-17 23:42:19 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -2,7 +2,11 @@ class Course
   attr_accessor :title, :schedule, :description
   @@all = []
 
-
+  def initialize(name, schedule, description)
+    @name = name
+    @schedule = schedule
+    @description = description
+  end
 
   def self.all
     @@all
